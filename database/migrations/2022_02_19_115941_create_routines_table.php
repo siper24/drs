08540b80fc8fd5c80981ba508a9f0841ca670bad
siper24 <angelonavalta24@gmail.com>
2022-03-28 14:06:39 +0800

    view by id, delete by id, edit by id and search

--- database/migrations/2022_02_19_115941_create_routines_table.php
+++ database/migrations/2022_02_19_115941_create_routines_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('routines', function (Blueprint $table) {
             $table->id();
             $table->text('name');
-            $table->longText('description')->nullable();
+            $table->longText('description')->nulla();
             $table->timestamps();
         });
     }
